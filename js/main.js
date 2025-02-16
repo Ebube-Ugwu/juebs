6fb033bf3393ef4353b09412025859a3b19dfcc6
Ebube Ugwu <ugwuebube3@gmail.com>
2025-02-16 19:03:17 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function initializeCollapsibles () {
+	const navToggler = document.querySelector(".nav__toggler");
+
+	function toggleNav() {
+		const nav = document.querySelector("nav");
+		nav.classList.toggle("collapsible--expanded");
+	}
+
+	navToggler.addEventListener("click", toggleNav);	
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+	initializeCollapsibles();
+})
