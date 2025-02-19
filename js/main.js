4cda6d6c837f21c385621164d6e41472ce45f4c0
Ebube Ugwu <ugwuebube3@gmail.com>
2025-02-19 16:10:05 +0100

    feat: add accordion component

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
 function initializeCollapsibles () {
 	const navToggler = document.querySelector(".nav__toggler");
+	const collapsibles = document.querySelectorAll(".collapsible");
 
 	function toggleNav() {
 		const nav = document.querySelector("nav");
 		nav.classList.toggle("collapsible--expanded");
 	}
 
-	navToggler.addEventListener("click", toggleNav);	
+	function toggleCollapsibles() {
+		collapsibles.forEach( (item) => {
+			item.addEventListener("click", () => {
+				item.classList.toggle("collapsible--expanded");
+			});
+		});
+	}
+
+
+	toggleCollapsibles();
+	// navToggler.addEventListener("click", toggleNav);	
 }
 
 
 document.addEventListener("DOMContentLoaded", () => {
-	// initializeCollapsibles();
+	initializeCollapsibles();
 })
